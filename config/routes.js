6fb020b0e04ec8180881c6ed522b54af724a87df
Fabio <fspirola@gmail.com>
2018-06-09 17:56:27 -0300

    backend v1

--- config/routes.js
+++ config/routes.js
@@ -8,7 +8,10 @@ module.exports = function(server) {
   server.use('/api', router)
 
   // Rotas da API
-  const billingCycleService = require('../api/BillingCycle/billingCycleService')
+  const billingCycleService = require('../api/billingCycle/billingCycleService')
   billingCycleService.register(router, '/billingCycles')
+
+  const billingSummaryService = require('../api/billingSummary/billingSummaryService')
+  router.route('/billingSummary').get(billingSummaryService.getSummary)
 
 }
